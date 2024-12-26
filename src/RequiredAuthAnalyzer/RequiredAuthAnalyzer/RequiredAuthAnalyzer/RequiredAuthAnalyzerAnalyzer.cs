682cfc0a6d39ae52332d90ada41177da40be3daf
Al Rodriguez <ProgrammerAl@outlook.com>
2024-12-25 17:39:03 -0800

    Interrim commit, just to get some files checked in. Not much functionality

--- src/RequiredAuthAnalyzer/RequiredAuthAnalyzer/RequiredAuthAnalyzer/RequiredAuthAnalyzerAnalyzer.cs
+++ src/RequiredAuthAnalyzer/RequiredAuthAnalyzer/RequiredAuthAnalyzer/RequiredAuthAnalyzerAnalyzer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
+
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
@@ -35,11 +36,18 @@ namespace RequiredAuthAnalyzer
 
             // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
             // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
         }
 
         private static void AnalyzeSymbol(SymbolAnalysisContext context)
         {
+            var methodSymbol = (IMethodSymbol)context.Symbol;
+            if (IsEndpointForTraditionalController(methodSymbol))
+            {
+                AnalyzeTraditionalControllerEndpoint(methodSymbol, context);
+            }
+
+
             // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
             var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
 
@@ -51,6 +59,43 @@ namespace RequiredAuthAnalyzer
 
                 context.ReportDiagnostic(diagnostic);
             }
+        }
+
+        private static void AnalyzeTraditionalControllerEndpoint(IMethodSymbol methodSymbol, SymbolAnalysisContext context)
+        {
+            //TODO: Are there more attributes?
+            var hasAuthAttribute = methodSymbol.GetAttributes()
+                .Any(x =>
+                {
+                    var name = x.GetType().FullName;
+                    return name == "Microsoft.AspNetCore.Authorization.AuthorizeAttribute"
+                        || name == "Microsoft.AspNetCore.Authorization.AllowAnonymousAttribute";
+                });
+
+            if (!hasAuthAttribute)
+            {
+                var diagnostic = Diagnostic.Create(Rule, methodSymbol.Locations[0], methodSymbol.Name);
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private static bool IsEndpointForTraditionalController(IMethodSymbol methodSymbol)
+        {
+            var parentClass = methodSymbol.ContainingType;
+            var isParentApiController = parentClass
+                    .GetAttributes()
+                    .Any(x => x.GetType().FullName == "Microsoft.AspNetCore.Mvc.ApiControllerAttribute");
+
+            if (!isParentApiController)
+            {
+                return false;
+            }
+
+            var isMethodAnEndpoint = methodSymbol.DeclaredAccessibility == Accessibility.Public
+                && methodSymbol.IsStatic == false
+                && methodSymbol.MethodKind == MethodKind.Ordinary;
+
+            return isMethodAnEndpoint;
         }
     }
 }
